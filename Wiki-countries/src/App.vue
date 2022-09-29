d985075cf215038ef3e39ba9a24f690e0e442fe1
Pabl0Parra <pabsblue16@gmail.com>
2022-09-29 20:23:36 +0200

    added Bootstrap5

--- Wiki-countries/src/App.vue
+++ Wiki-countries/src/App.vue
@@ -1,8 +1,11 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
+import Navbar from "./components/Navbar.vue";
 </script>
 
 <template>
+  <Navbar />
+  <div class="hello"></div>
   <RouterView />
 </template>
 

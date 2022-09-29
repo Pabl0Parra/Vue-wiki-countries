df641f1383c4337953f564ac55cf7e250c2cd12a
Pabl0Parra <pabsblue16@gmail.com>
2022-09-29 20:36:49 +0200

    created CountryList  component template

--- Wiki-countries/src/App.vue
+++ Wiki-countries/src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
 import Navbar from "./components/Navbar.vue";
+import CountriesList from "./components/CountriesList.vue";
 </script>
 
 <template>
   <Navbar />
+  <CountriesList />
   <div class="hello"></div>
   <RouterView />
 </template>
